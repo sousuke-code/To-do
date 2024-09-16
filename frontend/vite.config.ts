2395d5afe93fc4e187d427933dee33de2cf93a11
sousuke-code <162286022+sousuke-code@users.noreply.github.com>
2024-09-17 02:44:27 +0900

    intro tailwind

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build : {
-    assetsDir: 'assets',
-  }
-})
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    },
+  },
+});
